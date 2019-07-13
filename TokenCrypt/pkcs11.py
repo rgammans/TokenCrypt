10acf2dda0131b90b6aafc8bb047981b77c56f33
Roger Gammans <rgammans@gammascience.co.uk>
2019-07-13 16:01:19 +0100

    Add the rest of the basic instance __init__ tests.

--- TokenCrypt/pkcs11.py
+++ TokenCrypt/pkcs11.py
@@ -1,18 +1,39 @@
 import PyKCS11
+
+class MultipleMatchingTokens(LookupError):pass
+class NoMatchingTokens(LookupError):pass
+
 class Algorithm:
     def __init__(self, *args, **kwargs):
         self.lib = PyKCS11.PyKCS11Lib()
+        self.lib.load()
 
         self.slot = kwargs.get('slot', None)
         if self.slot is None:
-            self.slot = self._resolve_slot(**kwargs)
+            self.slot = self._resolve_slot(kwargs.get('token',{}))
 
-    def _resolve_slot(self, **data):
-        slots = self.lib.get_slots()
+    def _resolve_slot(self, tokenspec):
+        slots = self.lib.getSlotList()
         found = None
         for s in slots:
             inf = self.lib.get_tokeninfo(s)
+            ## Search through all the 
+            #  provided data for a match with the token
+            #  specified.
+            for key in tokenspec.keys():
+                if inf.get(key,None) != tokenspec[key]:
+                    break
+            else:
+                ## If we find a match; see if we a have
+                #  already found one as we want a unique
+                #  match
+                if found is not None:
+                    raise MultipleMatchingTokens(found,s)
+                found = s
 
+        #Check we have a match
+        if found is None:
+            raise NoMatchingTokens(tokenspec)
         return found
 
 
--- tests/test_pkcss11.py
+++ tests/test_pkcss11.py
@@ -8,11 +8,13 @@ class PyKCSS11_Classtests(unittest.TestCase):
 
 
     def test_class_accepts_a_slot(self,):
-        out = mut.Algorithm(slot = unittest.mock.sentinel.SLOT )
+        with unittest.mock.patch('PyKCS11.PyKCS11Lib' ):
+            out = mut.Algorithm(slot = unittest.mock.sentinel.SLOT )
         self.assertEqual(out.slot, unittest.mock.sentinel.SLOT )
 
     def test_class_accepts_zero_as_valid_slot(self,):
-        with unittest.mock.patch.object(mut.Algorithm,'_resolve_slot', 
+        with unittest.mock.patch('PyKCS11.PyKCS11Lib' ) as x,\
+             unittest.mock.patch.object(mut.Algorithm,'_resolve_slot', 
             return_value=unittest.mock.sentinel.SLOT ) as x:
 
             out = mut.Algorithm(slot = 0 )
@@ -27,25 +29,105 @@ class PyKCSS11_Classtests(unittest.TestCase):
         x.assert_called_once()
 
     def test_class_accepts_a_token_argument_to_match_info_against(self,):
-        with unittest.mock.patch.object(mut.Algorithm, '_resolve_slot', return_value=unittest.mock.sentinel.SLOTX ):
+        with unittest.mock.patch('PyKCS11.PyKCS11Lib' ) as x,\
+             unittest.mock.patch.object(mut.Algorithm, '_resolve_slot', return_value=unittest.mock.sentinel.SLOTX ):
             out = mut.Algorithm(token = unittest.mock.sentinel.TOKEN1 )
         self.assertEqual(out.slot, unittest.mock.sentinel.SLOTX )
 
     def test_dunder_resolve_slot_gets_list_of_slots_the_slot_data(self,):
         lib =  unittest.mock.MagicMock()
         with unittest.mock.patch('PyKCS11.PyKCS11Lib', return_value = lib):
-            out = mut.Algorithm(token = unittest.mock.sentinel.TOKEN2 )
-        lib.get_slots.assert_called_once()
+            try:
+                out = mut.Algorithm(token = unittest.mock.sentinel.TOKEN2 )
+            except LookupError:pass
+        lib.getSlotList.assert_called_once()
+
+    def test_dunder_resolve_slot_gets_token_info_from_all_slots_the_slot_data(self,):
+        avail_slots = list(range(10))
+
+        lib =  unittest.mock.MagicMock()
+        lib.get_tokeninfo = unittest.mock.MagicMock(return_value ={})
+        lib.getSlotList = unittest.mock.MagicMock(return_value = avail_slots)
+        with unittest.mock.patch('PyKCS11.PyKCS11Lib', return_value = lib):
+            try:
+                out = mut.Algorithm(token = {'x':unittest.mock.sentinel.TOKEN2} )
+            except LookupError:pass
+        lib.get_tokeninfo.assert_has_calls([ unittest.mock.call(s) for s in  avail_slots] )
+
+    def test_dunder_resolve_slot_choses_a_slot_if_only_the_index_one_matches(self,):
+
+        slot_data =  [ {'xyzzy':1}, {'serialnr':'123','manufacturer':'acme'} ]
+        avail_slots = range(len(slot_data))
+
+        lib =  unittest.mock.MagicMock()
+        lib.getSlotList = unittest.mock.MagicMock(return_value = avail_slots)
+        lib.get_tokeninfo = unittest.mock.MagicMock( side_effect = slot_data )
+        with unittest.mock.patch('PyKCS11.PyKCS11Lib', return_value = lib):
+            out = mut.Algorithm(token = {'serialnr':'123','manufacturer':'acme'} )
+
+        lib.get_tokeninfo.assert_has_calls([ unittest.mock.call(s) for s in  avail_slots] )
+        self.assertEqual(out.slot, 1 )
 
 
+    def test_dunder_resolve_slot_choses_slot_0_if_only_index_zero_matches(self,):
 
-    @unittest.skip('nyi')
-    def test_class_searches_for_slor_if_givev_a_dict(self,):
-        self.fail()
+        slot_data =  [{'serialnr':'456','manufacturer':'acme'}, {'serialnr':'123','manufacturer':'acme'} ]
+        avail_slots = range(len(slot_data))
 
-    @unittest.skip('nyi')
-    def test_class_raises_an_exeption_if_slot_not_matched(self,):
-        self.fail()
+        lib =  unittest.mock.MagicMock()
+        lib.getSlotList = unittest.mock.MagicMock(return_value = avail_slots)
+        lib.get_tokeninfo = unittest.mock.MagicMock( side_effect = slot_data )
+        with unittest.mock.patch('PyKCS11.PyKCS11Lib', return_value = lib):
+            out = mut.Algorithm(token = {'serialnr':'456','manufacturer':'acme'} , )
+
+        lib.get_tokeninfo.assert_has_calls([ unittest.mock.call(s) for s in  avail_slots] )
+        self.assertEqual(out.slot, 0 )
+
+    def test_dunder_resolve_slot_raises_if_there_are_mutiple_matches(self,):
+
+        slot_data =  [{'serialnr':'456','manufacturer':'acme'}, {'serialnr':'456','manufacturer':'acme'} ]
+        avail_slots = range(len(slot_data))
+
+        lib =  unittest.mock.MagicMock()
+        lib.getSlotList = unittest.mock.MagicMock(return_value = avail_slots)
+        lib.get_tokeninfo = unittest.mock.MagicMock( side_effect = slot_data )
+        with unittest.mock.patch('PyKCS11.PyKCS11Lib', return_value = lib):
+            with self.assertRaises(mut.MultipleMatchingTokens):
+                out = mut.Algorithm(token = {'serialnr':'456','manufacturer':'acme'} , )
+
+    def test_dunder_resolve_slot_raises_if_there_are_No_matches(self,):
+        slot_data =  [{'serialnr':'456','manufacturer':'acme'}, {'serialnr':'456','manufacturer':'acme'} ]
+        avail_slots = range(len(slot_data))
+
+        lib =  unittest.mock.MagicMock()
+        lib.getSlotList = unittest.mock.MagicMock(return_value = avail_slots)
+        lib.get_tokeninfo = unittest.mock.MagicMock( side_effect = slot_data )
+        with unittest.mock.patch('PyKCS11.PyKCS11Lib', return_value = lib):
+            with self.assertRaises(mut.NoMatchingTokens):
+                out = mut.Algorithm(token = {'serialnr':'12','manufacturer':'acme'} , )
+
+
+    def test_dunder_resolve_slot_choses_slot_1_0_only_the_index_zero_an_obsuce_tag(self,):
+
+        slot_data =  [ {'xyzzy':1}, {'serialnr':'123','manufacturer':'acme'} ]
+        avail_slots = range(len(slot_data))
+
+        lib =  unittest.mock.MagicMock()
+        lib.getSlotList = unittest.mock.MagicMock(return_value = avail_slots)
+        lib.get_tokeninfo = unittest.mock.MagicMock( side_effect = slot_data )
+        with unittest.mock.patch('PyKCS11.PyKCS11Lib', return_value = lib):
+            out = mut.Algorithm(token = {'xyzzy':1} )
+
+        lib.get_tokeninfo.assert_has_calls([ unittest.mock.call(s) for s in  avail_slots] )
+        self.assertEqual(out.slot, 0 )
+
+
+    def test_class_ctor_loads_the_wrapped_library(self,):
+         lib  = unittest.mock.MagicMock()
+         with unittest.mock.patch('PyKCS11.PyKCS11Lib', return_value = lib) as x:
+            out = mut.Algorithm(slot=0 )
+
+         lib.load.assert_called_once()
 
 
     @unittest.skip('nyi')
