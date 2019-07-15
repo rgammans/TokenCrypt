a60713106aa6c37c2bb0817a012e0a07254e6304
Roger Gammans <rgammans@gammascience.co.uk>
2019-07-15 15:48:48 +0100

    Change to using setup.py for deps as is library
    Pipenv is only for applications.

---
+++ setup.py
@@ -0,0 +1,46 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='tokencrypt',
+    version='0.1.0',
+    description='PKCS11 Keys classes for cryptography',
+    long_description=long_description,
+    #url='https://github.com/gammascience/pricert',
+    author='Gamma Science Ltd',
+    author_email='rgammans@gammascience.co.uk',
+    classifiers=[  # Optional
+        'Development Status :: 3 - Alpha',
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        'Topic :: Utilities',
+        'Topic :: Security',
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+        #Version of Python we know work.
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='pkcs11 security cryptography',
+
+    packages=find_packages(exclude=['docs', 'tests']),
+    install_requires=[
+        'cryptography',
+        'PyKCS11'
+    ],
+    extras_require={
+        'dev': ['pipenv',''],
+        'test': ['coverage'],
+    },
+    test_suite = 'tests',
+
+)
